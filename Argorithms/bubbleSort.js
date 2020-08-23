0726ee78a18733fc0388793be3a2a8eebde2373e
JudyLime <56984748+judylime@users.noreply.github.com>
2020-08-22 23:48:44 -0700

    bubbleList"

---
+++ Argorithms/bubbleSort.js
@@ -0,0 +1,25 @@
+//创建列表类
+function ArrayList(){
+  // 属性
+  this.array =[]
+  
+  //方法
+  //将数据可以插入到数组中的方法
+  ArrayList.prototype.insert = function (item) {
+    this.array
+  }
+}
+
+
+// toString
+
+//实现排序算法
+
+//冒泡排序
+
+
+
+// 1 获取数组的长度
+// 第一次进来： i=0， 比较0和1位置的两个数据， 如果0位置大于1未知的数据
+
+// 交换两个位置
